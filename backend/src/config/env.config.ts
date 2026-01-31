f8604ae14d9b851d22d82e6a1b28778c4cb37bb5
deepansh <mauryadeepansh40@gmail.com>
2026-01-31 16:17:46 +0530

    refactor: remove NODE_ENV from environment configuration

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -1,7 +1,6 @@
 import { getEnv } from '../utils/get-env';
 
 const envConfig = () => ({
-  NODE_ENV: getEnv('NODE_ENV', 'development'),
   PORT: getEnv('PORT', '3000'),
 
   BASE_PATH: getEnv('BASE_PATH', '/api'),
